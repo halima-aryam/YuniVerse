2a52e75cc5529d755baa2e5519bb6bd4cb24248f
halima-aryam <salaammaryamm@gmail.com>
2026-04-26 13:17:57 +0300

    Update fonts to Elsie and Nunito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;1,400&family=Outfit:wght@300;400;500;600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Elsie:wght@400;900&family=Nunito:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
       </head>
       <body suppressHydrationWarning>
         <ThemeProvider>
